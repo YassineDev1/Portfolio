7f3db3db5d26c2822ee989a710b03d66baa9bd40
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-12-19 23:13:00 +0100

    media queries resume component

--- components/Resume.js
+++ components/Resume.js
@@ -1,11 +1,11 @@
 export default function Resume() {
   return (
     <div className="p-10 text-center">
-      <h2 className="py-2 text-5xl font-medium text-blue-700 ">
+      <h2 className="py-2 text-5xl font-medium text-blue-700 md:text-6xl ">
         Yassine EL MHERZI
       </h2>
-      <h3 className="py-2 text-2xl">Web Developer</h3>
-      <p className="py-5 leading-8 text-gray-800 text-md ">
+      <h3 className="py-2 text-2xl md:text-3xl">Web Developer</h3>
+      <p className="max-w-xl py-5 mx-auto leading-8 text-gray-800 text-md md:text-xl ">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio eveniet
         eaque cupiditate. Itaque recusandae culpa libero dolores odio quibusdam
         velit.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
 import Resume from "../components/Resume";
 import Services from "../components/Services";
 import SocialMedia from "../components/SocialMedia";
@@ -14,12 +15,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="px-10 bg-white ">
+      <main className="px-10 bg-white md:px-20 lg:px-40 ">
         <section className="min-h-screen">
           <Navbar />
           <Resume />
           <SocialMedia />
           <Services />
+          <Projects />
         </section>
       </main>
     </>
