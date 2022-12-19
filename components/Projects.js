d8fccf82ff6fd52a1530d6172deae4ee664d2fdc
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-12-19 23:25:57 +0100

    create and style  Projects Component

---
+++ components/Projects.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import ref1 from "../public/myDoctor.PNG"
+import ref2 from "../public/myDoctor1.PNG";
+import ref3 from "../public/myDoctor2.PNG";
+import ref4 from "../public/myDoctor3.PNG";
+export default function Projects() {
+  return (
+    <section>
+      <div>
+        <h3 className="py-1 text-3xl">Portfolio</h3>
+        <p className="py-2 leading-8 text-gray-800 text-md">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum,
+          explicabo? Possimus numquam quam hic aliquid voluptate magnam nobis
+          reiciendis sint. Reiciendis placeat rem autem provident ex dolorum
+          iure incidunt libero!
+        </p>
+      </div>
+      <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+        <div className="flex-1 basis-1/3">
+          <Image src={ref1} className="object-cover rounded-lg shadow-lg" width={'100%'} height={'100%'} layout="responsive"  />
+        </div>
+        <div className="flex-1 basis-1/3">
+          <Image src={ref2} className="object-cover rounded-lg shadow-lg" width={'100%'} height={'100%'} layout="responsive" />
+        </div>
+        <div className="flex-1 basis-1/3">
+          <Image src={ref3} className="object-cover rounded-lg shadow-lg" width={'100%'} height={'100%'} layout="responsive" />
+        </div>
+        <div className="flex-1 basis-1/3">
+          <Image src={ref4} className="object-cover rounded-lg shadow-lg" width={'100%'} height={'100%'} layout="responsive" />
+        </div>
+      </div>
+    </section>
+  );
+}
