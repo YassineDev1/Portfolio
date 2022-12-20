669492af7bfd266393ffec8f30d65ffff7b44382
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-12-20 20:58:28 +0100

    add github icon to social media component

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -2,6 +2,7 @@ import {
   AiFillLinkedin,
   AiFillInstagram,
   AiFillFacebook,
+  AiFillGithub
 } from "react-icons/ai";
 
 export default function SocialMedia() {
@@ -9,6 +10,7 @@ export default function SocialMedia() {
     <div className="flex justify-center gap-16 py-3 text-4xl text-gray-600">
       <AiFillFacebook className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
       <AiFillLinkedin className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
+      <AiFillGithub className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
       <AiFillInstagram className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
     </div>
   );
