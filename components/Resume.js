5d83d9779ca46cd97b1c20c30eafbf82aaf05928
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-12-22 00:25:14 +0100

    change text size and height of typewrapperTypewriter

--- components/Resume.js
+++ components/Resume.js
@@ -10,10 +10,10 @@ export default function Resume() {
           loop: false, 
           
           wrapperClassName:
-            "max-w-xl py-2 text-4xl font-medium text-blue-700 md:text-6xl ",
+            "max-w-xl py-2 text-3xl font-medium text-blue-700 md:text-5xl ",
           cursor: "",
           cursorClassName:
-            "max-w-xl py-2 text-4xl font-medium text-blue-700 md:text-6xl ",
+            "max-w-xl py-2 text-3xl font-medium text-blue-700 md:text-5xl ",
         }}
       />
       <Typewriter
@@ -23,9 +23,9 @@ export default function Resume() {
           strings: ["MERN Stack Developer", "Java Developer", "PHP Developer"],
           autoStart: true,
           loop: true,
-          wrapperClassName: "py-2 text-2xl md:text-3xl dark:text-white",
+          wrapperClassName: "py-2 text-xl md:text-2xl dark:text-white",
           cursor: "",
-          cursorClassName: "py-2 text-2xl md:text-3xl dark:text-white",
+          cursorClassName: "py-2 text-xl md:text-2xl dark:text-white",
         }}
       />
       <p className="max-w-xl py-5 mx-auto leading-8 text-gray-800 dark:text-gray-300 text-md md:text-xl ">
