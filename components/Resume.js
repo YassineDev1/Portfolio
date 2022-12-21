e34ac6c77ac01e3e859dfc4551b3de78f5b837f7
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-12-21 22:38:09 +0100

    add typewriter to Resume Component

--- components/Resume.js
+++ components/Resume.js
@@ -1,10 +1,33 @@
+import Typewriter from "typewriter-effect";
+
 export default function Resume() {
   return (
-    <div className="p-10 text-center">
-      <h2 className="max-w-xl py-2 text-4xl font-medium text-blue-700 md:text-6xl ">
-        Yassine EL MHERZI
-      </h2>
-      <h3 className="py-2 text-2xl md:text-3xl dark:text-white">Web Developer</h3>
+    <div className="p-10 text-center ">
+      <Typewriter
+        options={{
+          strings: "Yassine EL MHERZI",
+          autoStart: true,
+          loop: false, 
+          
+          wrapperClassName:
+            "max-w-xl py-2 text-4xl font-medium text-blue-700 md:text-6xl ",
+          cursor: "",
+          cursorClassName:
+            "max-w-xl py-2 text-4xl font-medium text-blue-700 md:text-6xl ",
+        }}
+      />
+      <Typewriter
+        className="py-2 text-2xl md:text-3xl dark:text-white"
+  
+        options={{
+          strings: ["MERN Stack Developer", "Java Developer", "PHP Developer"],
+          autoStart: true,
+          loop: true,
+          wrapperClassName: "py-2 text-2xl md:text-3xl dark:text-white",
+          cursor: "",
+          cursorClassName: "py-2 text-2xl md:text-3xl dark:text-white",
+        }}
+      />
       <p className="max-w-xl py-5 mx-auto leading-8 text-gray-800 dark:text-gray-300 text-md md:text-xl ">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio eveniet
         eaque cupiditate. Itaque recusandae culpa libero dolores odio quibusdam
