aeb862eed7e9c7b3b87a983c29042d722a66eec6
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-12-21 23:52:40 +0100

    validate form using yup and formik

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,19 +1,33 @@
 import { useFormik } from "formik";
+import * as Yup from "yup";
 import Image from "next/image";
 
 export default function Contact() {
-    const formik = useFormik({
-        initialValues: {
-            name:'',
-            email:'',
-            country: 'MA',
-            terms: ''
-        },
-       
-    })
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      country: "MA",
+      terms: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(20, "Name must be less 20 charracter")
+        .required("Name is required"),
+      email: Yup.string()
+        .email("Invalid email adress")
+        .required("Email is required"),
+       country: Yup.string().required("Counntry is required"), 
+       terms: Yup.array().required("Terms of service must be checked")
+    }),
+    onSubmit: (values) => console.log(values)
+  });
   return (
     <main className="flex items-center justify-center h-screen bg-blue-500">
-      <form onSubmit={formik.handleSubmit} className="flex w-2/3 font-sans bg-white rounded-lg">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="flex w-2/3 font-sans bg-white rounded-lg"
+      >
         <div className="flex-1 p-20 text-gray-700">
           <h1 className="pb-2 text-3xl text-blue-800">Let's get started</h1>
           <p className="text-lg text-gray-600">
@@ -23,8 +37,17 @@ export default function Contact() {
           <div className="mt-6">
             {/* Name input field */}
             <div className="pb-4">
-              <label className="block pb-2 text-sm" htmlFor="name">
-                Name
+              <label
+                className={`block pb-2 text-sm ${
+                  formik.touched.name && formik.errors.name
+                    ? "text-red-600"
+                    : ""
+                }`}
+                htmlFor="name"
+              >
+                {formik.touched.name && formik.errors.name
+                  ? formik.errors.name
+                  : "Name"}
               </label>
               <input
                 className="w-1/2 p-2 border-2 border-gray-500 rounded-md outline-none focus:border-blue-500 focus:ring-blue-500"
@@ -32,13 +55,23 @@ export default function Contact() {
                 name="name"
                 value={formik.values.name}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 placeholder="Enter your name"
               />
             </div>
             {/* Email input field */}
             <div className="pb-4">
-              <label className="block pb-2 text-sm" htmlFor="email">
-                Email
+              <label
+                className={`block pb-2 text-sm ${
+                  formik.touched.name && formik.errors.name
+                    ? "text-red-600"
+                    : ""
+                }`}
+                htmlFor="email"
+              >
+                {formik.touched.email && formik.errors.email
+                  ? formik.errors.email
+                  : "Email"}
               </label>
               <input
                 className="w-1/2 p-2 border-2 border-gray-500 rounded-md outline-none focus:border-blue-500 focus:ring-blue-500"
@@ -51,8 +84,17 @@ export default function Contact() {
             </div>
             {/* Country input field */}
             <div className="pb-4">
-              <label className="block pb-2 text-sm" htmlFor="country">
-                Country
+              <label
+                className={`block pb-2 text-sm ${
+                  formik.touched.name && formik.errors.name
+                    ? "text-red-600"
+                    : ""
+                }`}
+                htmlFor="country"
+              >
+                {formik.touched.country && formik.errors.country
+                  ? formik.errors.country
+                  : "Country"}
               </label>
               <select
                 name="country"
@@ -69,8 +111,17 @@ export default function Contact() {
             </div>
             {/* terms of service*/}
             <div className="pb-4">
-              <label className="block pb-2 text-sm" htmlFor="terms">
-                Terms of service
+              <label
+                className={`block pb-2 text-sm ${
+                  formik.touched.name && formik.errors.name
+                    ? "text-red-600"
+                    : ""
+                }`}
+                htmlFor="terms"
+              >
+                {formik.touched.terms && formik.errors.terms
+                  ? formik.errors.terms
+                  : "Terms of service"}
               </label>
               <div className="flex items-center gap-2">
                 <input
