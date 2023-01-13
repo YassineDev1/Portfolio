3988eee0bfe9ebc1d4a6b02620bbd2fdd9aaf154
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-01-13 23:12:09 +0100

    create data.js contains cards info

---
+++ data/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: 1,
+    project: "Landing Pages",
+    technology: "React",
+    text: "I use React, Tailwind, and other to implement landing pages",
+    tools: ["ReactJS", "Tailwindcss", "NodeJS"],
+  },
+  {
+    id: 2,
+    project: "Desktop Apps",
+    technology: "Vue",
+    text: "I use C++, Java, and other to implement Desktop pages",
+    tools: ["C++", "Java", "NGINX"],
+  },
+  {
+    id: 3,
+    project: "Mobile Apps",
+    technology: "Docker",
+    text: "I use ReactNative, Docker, and other to implement Desktop pages",
+    tools: ["ReactNative", "Kotlin", "Docker"],
+  },
+  {
+    id: 4,
+    project: "Custom software",
+    technology: "Laravel",
+    text: "I use Angular, Laravel, and other to implement Desktop pages",
+    tools: ["Angular", "Tailwindcss", "Laravel8"],
+  },
+];
