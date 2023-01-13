1249bf4948aa15a301415531d3135a51dfb1fa33
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-01-13 21:25:59 +0100

    link social Media icons to their urls

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -7,18 +7,42 @@ import {
 import {motion as m} from "framer-motion"
 
 export default function SocialMedia() {
+
+  const Redirect = (url) => {
+    window.open(url, "_blank")
+  }
   return (
     <div className="flex justify-center gap-16 py-3 text-4xl text-gray-600">
-      <m.span whileHover={{ scale: 1.5 }}>
+      <m.span
+        onClick={() => Redirect("https://facebook.com/yassine.elmherzi/")}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 0.6 }}
+        whileHover={{ scale: 1.5 }}
+      >
         <AiFillFacebook className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
       </m.span>
-      <m.span whileHover={{ scale: 1.5 }}>
+      <m.span
+        onClick={() => Redirect("https://www.linkedin.com/in/yassine-elmherzi")}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 0.6 }}
+        whileHover={{ scale: 1.5 }}
+      >
         <AiFillLinkedin className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
       </m.span>
-      <m.span whileHover={{ scale: 1.5 }}>
+      <m.span
+        onClick={() => Redirect("https://github.com/yassineDev1/")}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 0.6 }}
+        whileHover={{ scale: 1.5 }}
+      >
         <AiFillGithub className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
       </m.span>
-      <m.span whileHover={{ scale: 1.5 }}>
+      <m.span
+        onClick={() => Redirect("https://instagram.com/yassine.elmherzi/")}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 0.6 }}
+        whileHover={{ scale: 1.5 }}
+      >
         <AiFillInstagram className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
       </m.span>
     </div>
