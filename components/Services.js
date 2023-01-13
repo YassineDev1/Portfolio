33afb4a3ce6e99ae17edc1943ac7643ce3078809
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-01-13 23:15:14 +0100

    clean the code and replace it with ServicesCards component

--- components/Services.js
+++ components/Services.js
@@ -1,8 +1,6 @@
 import Image from "next/legacy/image";
 import myPicture from "../public/myPicture.jpeg";
-import { FaReact, FaDocker, FaLaravel } from "react-icons/fa";
-import { RiVuejsFill } from "react-icons/ri";
-import {motion as m} from "framer-motion"
+import ServicesCards from "./ServicesCards";
 
 export default function Services() {
   return (
@@ -25,68 +23,7 @@ export default function Services() {
             Cupiditate, voluptate corrupti ipsam placeat est itaque?
           </p>
         </div>
-        <div className="gap-10 lg:flex">
-          <m.div
-            whileHover={{ scale: 1.1 }}
-            className="p-10 my-10 text-center shadow-lg rounded-xl dark:bg-white dark:text-gray-900"
-          >
-            <FaReact className="mx-auto text-6xl" />
-            <h3 className="pt-8 pb-2 text-lg font-medium">Landing Pages</h3>
-            <p className="py-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet,
-              alias!
-            </p>
-            <h4 className="text-blue-600 oy-4"> Tools Used</h4>
-            <p className="py-1 text-gray-800">ReactJs</p>
-            <p className="py-1 text-gray-800">TailwindCss</p>
-            <p className="py-1 text-gray-800">NodeJS</p>
-          </m.div>
-          <m.div
-            whileHover={{ scale: 1.1 }}
-            className="p-10 my-10 text-center shadow-lg rounded-xl dark:bg-white dark:text-gray-900"
-          >
-            <RiVuejsFill className="mx-auto text-6xl" />
-            <h3 className="pt-8 pb-2 text-lg font-medium">Desktop Apps</h3>
-            <p className="py-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet,
-              alias!
-            </p>
-            <h4 className="text-blue-600 oy-4"> Tools Used</h4>
-            <p className="py-1 text-gray-800">Rust</p>
-            <p className="py-1 text-gray-800">PHP</p>
-            <p className="py-1 text-gray-800">Docker</p>
-          </m.div>
-          <m.div
-            whileHover={{ scale: 1.1 }}
-            className="p-10 my-10 text-center shadow-lg rounded-xl dark:bg-white dark:text-gray-900"
-          >
-            <FaDocker className="mx-auto text-6xl" />
-            <h3 className="pt-8 pb-2 text-lg font-medium">Mobile Apps</h3>
-            <p className="py-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet,
-              alias!
-            </p>
-            <h4 className="text-blue-600 oy-4"> Tools Used</h4>
-            <p className="py-1 text-gray-800">Java</p>
-            <p className="py-1 text-gray-800">Kotlin</p>
-            <p className="py-1 text-gray-800">NGINX</p>
-          </m.div>
-          <m.div
-            whileHover={{ scale: 1.1 }}
-            className="p-10 my-10 text-center shadow-lg rounded-xl dark:bg-white dark:text-gray-900"
-          >
-            <FaLaravel className="mx-auto text-6xl" />
-            <h3 className="pt-8 pb-2 text-lg font-medium">Custom Software</h3>
-            <p className="py-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet,
-              alias!
-            </p>
-            <h4 className="text-blue-600 oy-4"> Tools Used</h4>
-            <p className="py-1 text-gray-800">AngularJS</p>
-            <p className="py-1 text-gray-800">TailwindCss</p>
-            <p className="py-1 text-gray-800">Laravel8</p>
-          </m.div>
-        </div>
+        <ServicesCards />
       </section>
     </>
   );
