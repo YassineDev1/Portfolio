efc93c4331c947f54b8bfcef1d27c81cac9cfd9e
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-01-13 21:09:07 +0100

    animate SocialMedia Icons

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -4,14 +4,23 @@ import {
   AiFillFacebook,
   AiFillGithub,
 } from "react-icons/ai";
+import {motion as m} from "framer-motion"
 
 export default function SocialMedia() {
   return (
     <div className="flex justify-center gap-16 py-3 text-4xl text-gray-600">
-      <AiFillFacebook className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
-      <AiFillLinkedin className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
-      <AiFillGithub className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
-      <AiFillInstagram className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
+      <m.span whileHover={{ scale: 1.5 }}>
+        <AiFillFacebook className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
+      </m.span>
+      <m.span whileHover={{ scale: 1.5 }}>
+        <AiFillLinkedin className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
+      </m.span>
+      <m.span whileHover={{ scale: 1.5 }}>
+        <AiFillGithub className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
+      </m.span>
+      <m.span whileHover={{ scale: 1.5 }}>
+        <AiFillInstagram className="rounded-full cursor-pointer hover:text-gray-900 dark:hover:text-gray-200 hover:rounded-full " />
+      </m.span>
     </div>
   );
 }
