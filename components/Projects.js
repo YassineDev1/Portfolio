1ad74aec5a10912a14a454143b0bc842d9c13e18
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-01-14 22:33:01 +0100

    add comming soon in the bottom

--- components/Projects.js
+++ components/Projects.js
@@ -1,20 +1,35 @@
+import Typewriter from "typewriter-effect"
 export default function Projects() {
   return (
-    <section>
+    <section id="about" className="py-12">
       <div className="text-center">
-        <h3 className="py-1 text-3xl dark:text-gray-50">Portfolio</h3>
+        <h3 className="py-1 text-3xl dark:text-gray-50">About Me</h3>
         <p className="py-2 leading-8 text-center text-gray-800 dark:text-gray-100 text-md">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum,
-          explicabo? Possimus numquam quam hic aliquid voluptate magnam nobis
-          reiciendis sint. Reiciendis placeat rem autem provident ex dolorum
-          iure incidunt libero!
+          I have a variety of projects that I have worked on, some of which are
+          available to view on this website. These projects showcase my skills,
+          experience and my ability to deliver high-quality work in a timely
+          manner.
         </p>
       </div>
-      <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+      {/* <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
         <div className="flex-1 basis-1/3"></div>
         <div className="flex-1 basis-1/3"></div>
         <div className="flex-1 basis-1/3"></div>
         <div className="flex-1 basis-1/3"></div>
+      </div> */}
+      <div className="flex justify-center py-20">
+        <Typewriter
+          options={{
+            strings: "Comming soon !",
+            autoStart: true,
+            loop: true,
+            wrapperClassName:
+              " py-2 text-center  mx-auto text-3xl font-medium text-red-500 md:text-5xl ",
+            cursor: "",
+            cursorClassName:
+              " py-2 text-3xl font-medium text-blue-700 md:text-5xl ",
+          }}
+        />
       </div>
     </section>
   );
