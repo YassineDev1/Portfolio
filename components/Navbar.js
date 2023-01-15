075eb847e3d1c433dc5f3135b6e4ac2eba9a94a6
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-01-15 14:27:36 +0100

    change text size and flex states in media queries in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,12 +4,12 @@ import { motion as m } from "framer-motion";
 
 export default function Navbar({ darkModeState, darkMode }) {
   return (
-    <m.nav className="flex justify-between w-full px-10 py-10 mb-12 shadow-md dark:shadow-gray-600 dark:shadow-sm md:px-20 lg:px-40 ">
+    <m.nav className="flex items-center justify-center w-full gap-8 px-6 py-10 mb-12 shadow-md md:gap-0 md:justify-between dark:shadow-gray-600 dark:shadow-sm md:px-20 lg:px-40 ">
       <m.h1
         initial={{ x: -500 }}
         animate={{ x: 0 }}
         transition={{ duration: 0.8 }}
-        className="text-xl font-burtons dark:text-[#cccccc] dark:text-semibold"
+        className="md:text-xl text-md font-burtons dark:text-[#cccccc] dark:text-semibold"
       >
         <Link href={"/"}>Yassine EL MHERZI</Link>
       </m.h1>
@@ -17,7 +17,7 @@ export default function Navbar({ darkModeState, darkMode }) {
         initial={{ x: 500 }}
         animate={{ x: 0 }}
         transition={{ duration: 0.8 }}
-        className="flex items-center justify-end text-lg  text-center  md:justify-between md:w-[450px]"
+        className="flex items-center justify-end   text-center  md:justify-between md:w-[450px]"
       >
         <li className="hidden px-2 link md:inline text-gray-900 dark:text-[#ccccce] dark:hover:text-[#ffffff] ">
           <Link href={"#about"}>About Me </Link>
@@ -33,7 +33,7 @@ export default function Navbar({ darkModeState, darkMode }) {
         </li>
         <li>
           <Link
-            className="px-4 py-2 ml-8 text-white rounded-md bg-gradient-to-r from-blue-500 to-blue-800"
+            className="px-4 py-2 ml-8 text-sm text-white rounded-md md:text-md bg-gradient-to-r from-blue-500 to-blue-800"
             href="https://www.linkedin.com/in/yassine-elmherzi"
             target="_blank"
             replace
