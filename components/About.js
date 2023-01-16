2f7ab6987dcb53d86376a29fc3d300f7f4896e69
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-01-16 18:14:50 +0100

    rewrite About component's style and animations

--- components/About.js
+++ components/About.js
@@ -1,36 +1,65 @@
-import Typewriter from "typewriter-effect"
+import Image from "next/image";
+import { motion as m } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useAnimation } from "framer-motion";
 export default function About() {
+  const animation = useAnimation();
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        opacity: 1,
+        transition: {
+          type: "keyframes",
+          duration: 1.2,
+          bounce: 0.3,
+        },
+      });
+    }
+    if (!inView) {
+      animation.start({
+        opacity: 0,
+      });
+    }
+  }, [inView, animation]);
+
   return (
-    <section id="about" className="py-12">
+    <m.section
+      animate={animation}
+      ref={ref}
+      id="about"
+      className="py-12 dark:text-gray-800 absolute left-0  bg-white w-full"
+    >
       <div className="flex flex-col items-center justify-center text-center">
-        <h3 className="py-1 text-3xl dark:text-gray-50">About Me</h3>
-        <p className="px-0 py-2 leading-8 text-center text-gray-800 md:px-12 md:w-2/3 dark:text-gray-100 text-md">
+        <h3 className="py-1 text-3xl ">About Me</h3>
+        <p className="px-0 py-2 leading-8 text-center text-gray-800 md:px-12 md:w-2/3text-md">
           I have a variety of projects that I have worked on, some of which are
           available to view on this website. These projects showcase my skills,
           experience and my ability to deliver high-quality work in a timely
           manner.
         </p>
       </div>
-      {/* <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-        <div className="flex-1 basis-1/3"></div>
-        <div className="flex-1 basis-1/3"></div>
-        <div className="flex-1 basis-1/3"></div>
-        <div className="flex-1 basis-1/3"></div>
-      </div> */}
-      <div className="flex justify-center py-20">
-        <Typewriter
-          options={{
-            strings: "Coming soon !",
-            autoStart: true,
-            loop: true,
-            wrapperClassName:
-              " py-2 text-center  mx-auto text-3xl font-medium text-red-500 md:text-5xl ",
-            cursor: "",
-            cursorClassName:
-              " py-2 text-3xl font-medium text-blue-700 md:text-5xl ",
-          }}
-        />
+      <div className="grid grid-cols-2 py-20">
+        <div className="border-r-2 border-solid  border-black">
+          <Image
+            src="/WebDev.png"
+            priority
+            width={700}
+            height={700}
+            alt="Web Dev"
+          />
+        </div>
+        <div className="flex text-xl font-semibold justify-center gap-12 flex-col items-center">
+          <h1 className="text-4xl">Collaborations </h1>
+          <div>Front End Developer</div>
+          <div>Backend Developer</div>
+          <div>DevOps Engineer</div>
+          <div>Data Scientist</div>
+        </div>
       </div>
-    </section>
+    </m.section>
   );
 }
